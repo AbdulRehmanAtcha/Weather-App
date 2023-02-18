480a561635b4e75a8bb1bf3874275006d6341080
AbdulRehmanAtcha <abdulrehman1239091@gmail.com>
2023-02-18 14:51:12 +0500

    Loader Added

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -9,6 +9,7 @@ import "react-toastify/dist/ReactToastify.css";
 function App() {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const toastHandler = (message) => {
     toast.info(message, {
@@ -26,25 +27,28 @@ function App() {
 
   const getWeatherHandler = (e) => {
     e.preventDefault();
+    setLoading(true);
     axios
       .get(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f5f221500244cb749828978d7d4e11a7`
       )
       .then((response) => {
-        
+        console.log(city)
+        setLoading(false);
         // console.log(response.data)
         setWeather(response?.data);
         console.log(response?.response);
         // console.log(data)
       })
       .catch((err) => {
-        console.log("Error", err?.response?.data?.message);
-        if(err?.response?.data?.message === "city not found" || err?.response?.data?.message === "Nothing to geocode"){
+        setLoading(false)
+        console.log("Error", err.message);
+        if(err?.response?.data?.message === "city not found"){
           // setShowError(true);
           toastHandler("No City Found")
         }
+        setWeather(null)
       });
-    setCity("");
   };
 
   return (
@@ -64,40 +68,54 @@ function App() {
           <button type="submit">Get Weather</button>
         </form>
       </div>
-      {weather === null? null : (
+
+      {(weather !== null && loading === true) ||
+      (weather === null && loading === true) ? (
+        <div className="loader">
+          <div class="spinner-grow text-light" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      ) : weather !== null && loading === false ? (
         <div className="boxes">
           <div className="box1">
             <h2>{Math.ceil(weather?.main?.temp - 273.15)}℃</h2>
             <img
-              style={{ height: "100px", width: "200px" }}
               className=""
               src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`}
               alt="Icon"
             />
             <h3>{weather?.weather[0].main}</h3>
+            <h3>
+              Feels Like: {Math.ceil(weather?.main?.feels_like - 273.15)}℃{" "}
+            </h3>
           </div>
           <div className="box2">
             <div className="parts">
               <h1>{weather?.name}</h1>
               <h2>
-                {weather?.sys?.country} &nbsp; {weather?.coord?.lon}&nbsp;{" "}
-                {weather?.coord?.lat}
+                {weather?.sys?.country}
+                <br />
+                Min: {Math.ceil(weather?.main?.temp_min - 273.15)}℃
+                <br />
+                Max: {Math.ceil(weather?.main?.temp_max - 273.15)}℃
               </h2>
             </div>
             <div className="parts">
-              <h3>
-                Feels Like: {Math.ceil(weather?.main?.feels_like - 273.15)}℃{" "}
-              </h3>
               <h3>Humidity: {weather?.main?.humidity}%</h3>
             </div>
             <div className="parts">
-              <button onClick={()=>{
-                toastHandler("Option available soon!")
-              }}>View More Details</button>
+              <button
+                onClick={() => {
+                  toastHandler("Option available soon!");
+                }}
+              >
+                View More Details
+              </button>
             </div>
           </div>
         </div>
-      )}
+      ) : null}
       <ToastContainer />
     </body>
   );
